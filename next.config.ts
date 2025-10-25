ac40b6f819b120d74c2d36f0b3f2cf0533c54a11
Remco Stoeten <remcostoeten@hotmail.com>
2025-10-26 00:18:37 +0200

    chore: remove experimental appDir support from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    // Enable src directory support
-    appDir: true,
-  },
 };
 
 export default nextConfig;
