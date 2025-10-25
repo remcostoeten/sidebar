6c8cb016c22f537aef2c0a32f2c07988e6888a4b
Remco Stoeten <remcostoeten@hotmail.com>
2025-10-25 17:22:29 +0200

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-neutral-950 antialiased`}
       >
         {children}
       </body>
